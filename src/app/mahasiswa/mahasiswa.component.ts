00d6bd3ef59be5b2f323bd778a7695dd26ffc9e3
Stevan Lim <165645924+stevanlim@users.noreply.github.com>
2024-12-06 11:20:22 +0700

    update: add new feature

--- src/app/mahasiswa/mahasiswa.component.ts
+++ src/app/mahasiswa/mahasiswa.component.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component } from '@angular/core';
 import DataTable from 'datatables.net-dt';
+import 'datatables.net-responsive-dt';
 
 @Component({
   selector: 'app-mahasiswa',
@@ -9,25 +9,58 @@ import DataTable from 'datatables.net-dt';
   templateUrl: './mahasiswa.component.html',
   styleUrl: './mahasiswa.component.css'
 })
-export class MahasiswaComponent implements AfterViewInit{
-  constructor(){}
+export class MahasiswaComponent implements AfterViewInit {
+  constructor() { }
   table: any = null;
+  data: any = null;
+  Object: Object = Object;
 
-  async getData(): Promise<void>{
+  async getData(): Promise<void> {
+    this.table.clear();
     const raw_data = await fetch("https://stmikpontianak.cloud/011100862/tampilMahasiswa.php");
-    const data : any = await raw_data.json();
-    
-    data.forEach((d: any) =>{
+    const data: any = await raw_data.json();
+    console.log(data);
+
+    data.forEach((d: any) => {
       const row = Object.keys(data[0]).map(key => d[key]);
       this.table.row.add(row);
     });
-
     this.table.draw(false);
+  }
+
+  Add(){
+    const nim = document.querySelector('input[name="Nim"]');
+    const nama = document.querySelector('input[name="Nama"]');
+    const alamat = document.querySelector('input[name="Alamat"]');
+    const jk = document.querySelector('select[name="JenisKelamin"]');
+    const tl = document.querySelector('input[name="TempatLahir"]');
+    const tgl = document.querySelector('input[name="TanggalLahir"]');
+    const jp = document.querySelector('select[name="JP"]');
+    const status = document.querySelector('select[name="StatusPernikahan"]');
+    const thn_msk = document.querySelector('input[name="TahunMasuk"]');
     
+    //@ts-ignore
+    fetch(`https://stmikpontianak.cloud/011100862/tambahMahasiswa.php?alamat=${encodeURIComponent(alamat.value)}&jenisKelamin=${encodeURIComponent(jk.value)}&jp=${encodeURIComponent(jp.value)}&nama=${encodeURIComponent(nama.value)}&nim=${encodeURIComponent(nim.value)}&statusPernikahan=${encodeURIComponent(status.value)}&tahunMasuk=${encodeURIComponent(thn_msk.value)}&tanggalLahir=${encodeURIComponent(tgl.value)}&tempatLahir=${encodeURIComponent(tl.value)}`)
+    .then(d => d.text())
+    .then(d =>{
+      this.getData();
+      //@ts-ignore
+      $("#exampleModal").modal("hide");
+      console.log("success to add data");
+    })
+    .catch(d =>{
+      alert("Gagal menambahkan data");
+    });
+
+
   }
 
   ngAfterViewInit(): void {
-    this.table = new DataTable('#table_id', {});
+
+    this.table = new DataTable('#table_id', {
+      responsive: true,
+     
+    });
     this.getData().then().catch(console.error);
   }
 }
